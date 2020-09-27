366331c4516bd86525a8cfb6b1ae87d1e00fd948
Kenzananas <bouzidkenza1@gmail.com>
2020-09-27 18:23:25 +0200

    better heuristic

--- minimax_assignment/utils.py
+++ minimax_assignment/utils.py
@@ -18,42 +18,41 @@ transposition_table = {}
 
 def order_children(children, player, desc):
     # sorted_children = sorted(children, key=lambda child: child.state.get_player_scores()[0] - child.state.get_player_scores()[1], reverse=desc)
-    sorted_children = sorted(children, key=lambda child: utility(player,child.state), reverse=desc)
+    sorted_children = sorted(children, key=lambda child: utility(child.state), reverse=desc)
     sorted_children = sorted(sorted_children, key=lambda child: child.move)
     return sorted_children
 
 def transition(node):
     return node.compute_and_get_children()
 
-def utility(player, state):
+
+def utility(state):
     fish_positions = state.fish_positions
     score_green, score_red = state.get_player_scores()
-    # chasing a good fish 
+    # chasing a good fish
     fish_scores = state.fish_scores
     best_score = max(fish_scores.values())
-    caugh_fish_id = state.player_caught[player]
+    caugh_fish_id = state.player_caught[0]
     if caugh_fish_id != -1 and best_score - fish_scores[caugh_fish_id] <= 2:
         return 100
-    # choose state with min distance to a good fish 
-    hook_position = state.hook_positions[player]
+    # choose state with min distance to a good fish
+    hook_position = state.hook_positions[0]
     min_distance = MAX_UTILITY
     best_fish_id = 0
     for fish_id in fish_positions:
         fish_position = fish_positions[fish_id]
-        d = (fish_position[0] - hook_position[0])**2 +(fish_position[1] - hook_position[1])**2
-        if best_score - fish_scores[fish_id] <= 5 and min_distance > d:
+        d = abs(fish_position[0] - hook_position[0]) + \
+            abs(fish_position[1] - hook_position[1])
+        if best_score - fish_scores[fish_id] <= 7 and min_distance > d:
             min_distance = d
             best_fish_id = fish_id
     # no good fish around
     if min_distance == MAX_UTILITY:
         return score_green - score_red
-    # take the min distance and weight with the best score 
-    coef = fish_scores[best_fish_id] if player == MAX else 1 / \
-        fish_scores[best_fish_id]
-    h = - coef * min_distance 
-    score = score_green - score_red if player == MAX else score_red - score_green 
+    # take the min distance and weight with the best score
+    h = - best_score * min_distance
     # print('heuristic', h)
-    return h + score
+    return h + score_green - score_red
 
 def minimax(node, depth, player, start_time):
     if player == MAX:
@@ -149,13 +148,12 @@ def negamax(node, depth, alpha, beta, player, start_time):
                 beta = min(beta, ttEntry.value)
     if alpha >= beta:
         return ttEntry['value'], node
-    if time.time() - start_time > MAX_ALLOWED_TIME_IN_SECONDS:
-        print('TIMEOUT in NEGAMAX', file=sys.stderr)
-        SEARCH_CUT_OFF = True
-        return float('-inf'), node
+    
+    # core of negamax
     children = order_children(transition(node), player, 1-player)
     if depth == 0 or not children:
-        return utility(player, node.state), None
+        color = 1 if player == MAX else -1
+        return color * utility(node.state), None
     nega_value, nega_move = float('-inf'), None
     for child in children:
         if time.time() - start_time > MAX_ALLOWED_TIME_IN_SECONDS:
@@ -170,6 +168,7 @@ def negamax(node, depth, alpha, beta, player, start_time):
         alpha = max(alpha, nega_value)
         if alpha >= beta: 
             break
+    # end of negamax 
 
     ttEntry['value'] = nega_value
     if nega_value <= alphaOrig:
